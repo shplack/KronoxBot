3b7161392a2d6bc9b574d1fc16bbd3bad3f256b2
shplack <shplack@shplack.tech>
2022-04-10 21:05:05 +0200

    refactor(database): courses
    added Database.Courses.* functions and localization functions
    
    Signed-off-by: shplack <shplack@shplack.tech>

--- database/kronox_db.py
+++ database/kronox_db.py
@@ -6,15 +6,15 @@ class Database:
     _conn: Connection
 
     @classmethod
-    def __init__(cls, path: str):
+    def __init__(cls, path: str) -> None:
         cls._conn = connect(path)
 
     @classmethod
-    def close(cls):
+    def close(cls) -> None:
         cls._conn.close()
 
     @classmethod
-    def query(cls, sql, *args):
+    def query(cls, sql, *args) -> None:
         cls._conn.execute(sql, args)
         cls._conn.commit()
 
@@ -23,23 +23,23 @@ class Database:
         return cls._conn.execute(sql, args).fetchone()
 
     @classmethod
-    def fetchall(cls, sql, *args):
+    def fetchall(cls, sql, *args) -> list[tuple] or list:
         return cls._conn.execute(sql, args).fetchall()
 
     @classmethod
-    def script(cls, sql):
+    def script(cls, sql) -> None:
         cls._conn.executescript(sql)
         cls._conn.commit()
 
     class Schools:
         @staticmethod
-        def add(name: str, acronym: str, link: str):
+        def add(name: str, acronym: str, link: str) -> None:
             query = 'INSERT INTO schools (name, acronym, link) VALUES (?, ?, ?);'
             Database.query(query, name, acronym, link)
 
         @staticmethod
         @cache
-        def all() -> dict:
+        def all() -> dict[str, str]:
             query = 'SELECT name, acronym FROM schools;'
             results = Database.fetchall(query)
             return {acronym: name for name, acronym in results}
@@ -61,13 +61,13 @@ class Database:
         class Localizations:
             @staticmethod
             def add(school: str, locale: str, localization: str) -> None:
-                query = 'INSERT INTO school_localization (school, locale, localization) VALUES (?, ?, ?);'
+                query = 'INSERT INTO school_localizations (school, locale, localization) VALUES (?, ?, ?);'
                 Database.query(query, school, locale, localization)
 
             @staticmethod
             @cache
-            def all() -> dict:
-                query = 'SELECT school, locale, localization FROM school_localization;'
+            def all() -> dict[str, dict[str, str]] or {}:
+                query = 'SELECT school, locale, localization FROM school_localizations;'
                 results = Database.fetchall(query)
 
                 localizations = {}
@@ -79,40 +79,40 @@ class Database:
 
             @staticmethod
             @cache
-            def by_locale(locale: str) -> list[tuple[str, str, str]]:
+            def by_locale(locale: str) -> list[tuple[str, str]] or None:
                 return Database.fetchall("""
                     SELECT acronym, localization 
-                    FROM school_localization 
+                    FROM school_localizations 
                     INNER JOIN schools
-                    ON school_localization.school = schools.name
+                    ON school_localizations.school = schools.name
                     WHERE locale = ?;
                 """, locale)
 
             @staticmethod
             @cache
-            def by_acronym(acronym: str) -> list[tuple[str, str, str]]:
+            def by_acronym(acronym: str) -> list[tuple[str, str, str]] or None:
                 return Database.fetchall("""
                     SELECT acronym, school, localization 
-                    FROM school_localization 
+                    FROM school_localizations 
                     INNER JOIN schools
-                    ON school_localization.school = schools.name
+                    ON school_localizations.school = schools.name
                     WHERE acronym = ?;
                 """, acronym)
 
             @staticmethod
             @cache
-            def by_school(school: str) -> list[tuple[str, str, str]]:
+            def by_school(school: str) -> list[tuple[str, str, str]] or None:
                 return Database.fetchall("""
                     SELECT acronym, school, localization 
-                    FROM school_localization 
+                    FROM school_localizations 
                     INNER JOIN schools
-                    ON school_localization.school = schools.name
+                    ON school_localizations.school = schools.name
                     WHERE school = ?;
                 """, school)
 
     class Programs:
         @staticmethod
-        def add(name: str, school: str, link: str):
+        def add(name: str, school: str, link: str) -> None:
             query = 'INSERT INTO programs (name, school, link) VALUES (?, ?, ?);'
             Database.query(query, name, school, link)
 
@@ -131,7 +131,7 @@ class Database:
 
         @staticmethod
         @cache
-        def by_school(school: str) -> list:
+        def by_school(school: str) -> list[str] or []:
             query = """
             SELECT programs.name FROM programs 
             INNER JOIN schools on programs.school = schools.name
@@ -142,13 +142,13 @@ class Database:
         class Localizations:
             @staticmethod
             def add(school: str, program: str, locale: str, localization: str) -> None:
-                query = 'INSERT INTO program_localization (school, program, locale, localization) VALUES (?, ?, ?, ?);'
+                query = 'INSERT INTO program_localizations (school, program, locale, localization) VALUES (?, ?, ?, ?);'
                 Database.query(query, school, program, locale, localization)
 
             @staticmethod
             @cache
-            def all() -> dict:
-                query = 'SELECT school, program, locale, localization FROM program_localization;'
+            def all() -> dict[str, dict[str, dict[str, str]]]:
+                query = 'SELECT school, program, locale, localization FROM program_localizations;'
                 results = Database.fetchall(query)
 
                 localizations = {}
@@ -162,8 +162,8 @@ class Database:
 
             @staticmethod
             @cache
-            def by_locale(locale: str) -> dict:
-                query = 'SELECT school, program, localization FROM program_localization WHERE locale = ?;'
+            def by_locale(locale: str) -> dict[str, dict[str, str]] or {}:
+                query = 'SELECT school, program, localization FROM program_localizations WHERE locale = ?;'
                 results = Database.fetchall(query, locale)
 
                 localizations = {}
@@ -175,8 +175,8 @@ class Database:
 
             @staticmethod
             @cache
-            def by_school(school: str) -> dict:
-                query = 'SELECT program, locale, localization FROM program_localization WHERE school = ?;'
+            def by_school(school: str) -> dict[str, dict[str, str]] or {}:
+                query = 'SELECT program, locale, localization FROM program_localizations WHERE school = ?;'
                 results = Database.fetchall(query, school)
 
                 localizations = {}
@@ -188,11 +188,80 @@ class Database:
 
             @staticmethod
             @cache
-            def by_school_locale(school: str, locale: str) -> list[tuple]:
+            def by_school_locale(school: str, locale: str) -> list[tuple[str, str]] or None:
                 query = """
                 SELECT program, localization
-                FROM program_localization 
-                INNER JOIN schools s on program_localization.school = s.name
+                FROM program_localizations 
+                INNER JOIN schools s on program_localizations.school = s.name
                 WHERE (school = ? OR acronym = ?) AND locale = ?;
                 """
                 return Database.fetchall(query, school, school, locale)
+
+        class Courses:
+            @staticmethod
+            def add(school: str, course: str, link: str) -> None:
+                query = 'INSERT INTO courses (name, link, school) VALUES (?, ?, ?);'
+                Database.query(query, school, course, link)
+
+            @staticmethod
+            @cache
+            def all() -> list[tuple[str, str]] or None:
+                query = 'SELECT school, name FROM courses;'
+                return Database.fetchall(query)
+
+            @staticmethod
+            @cache
+            def by_school(school: str) -> list[str] or None:
+                query = """
+                SELECT courses.name FROM courses
+                INNER JOIN schools on courses.school = schools.name
+                WHERE schools.name = ? OR schools.acronym = ?;
+                """
+                return Database.fetchall(query, school, school)
+
+            class Localizations:
+                @staticmethod
+                def add(school: str, course: str, locale: str, localization: str) -> None:
+                    query = """
+                    INSERT INTO course_localizations 
+                    (school, course, locale, localization) VALUES (?, ?, ?, ?);
+                    """
+                    Database.query(query, school, course, locale, localization)
+
+                @staticmethod
+                @cache
+                def all() -> dict[str, dict[str, dict[str, str]]]:
+                    query = 'SELECT school, course, locale, localization FROM course_localizations;'
+                    results = Database.fetchall(query)
+
+                    localizations = {}
+                    for school, course, locale, localization in results:
+                        if school not in localizations:
+                            localizations[school] = {}
+                        if course not in localizations[school]:
+                            localizations[school][course] = {}
+                        localizations[school][course][locale] = localization
+                    return localizations
+
+                @staticmethod
+                @cache
+                def by_locale(locale: str) -> dict[str, dict[str, str]]:
+                    query = 'SELECT school, course, localization FROM course_localizations;'
+                    results = Database.fetchall(query, locale)
+
+                    localizations = {}
+                    for school, course, localization in results:
+                        if school not in localizations:
+                            localizations[school] = {}
+                        localizations[school][course] = localization
+                    return localizations
+
+                @staticmethod
+                @cache
+                def by_school_locale(school: str, locale: str) -> list[tuple[str, str]] or None:
+                    query = """
+                    SELECT course, localization FROM course_localizations
+                    INNER JOIN schools on course_localizations.school = schools.name
+                    WHERE (schools.name = ? OR schools.acronym = ?) AND locale = ?;
+                    """
+                    return Database.fetchall(query, school, locale)
